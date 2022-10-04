c4aa2833512c621793c0209505d5dd655217bcdc
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-10-04 17:57:37 +0700

    cập nhật content

--- site/layout.php
+++ site/layout.php
@@ -1,32 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WE17311 - Lập trình website - FPT Poly</title>
+    <link rel="stylesheet" href="/we17311/content/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/we17311/content/css/style.css">
 </head>
+
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/we17311/site/index.php">Trang chủ</a>
-                </li>
-                <li>
-                    <a href="/we17311/site/index.php?san-pham">Danh sách sản phẩm</a>
-                </li>
-                <li>
-                    <a href="/we17311/site/index.php?gio-hang">Giỏ hàng</a>
-                </li>
-            </ul>
-        </nav>
-    </header>    
-    <main>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/we17311/site/index.php">Trang Chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/we17311/site/index.php?san-pham">Sản phẩm</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/we17311/site/index.php?gio-hang">Giỏ hàng</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
         <?php include_once $VIEW_NAME ?>
-    </main>
+    </div>
     <footer>
         Chân trang ở đây
     </footer>
 </body>
+
 </html>
